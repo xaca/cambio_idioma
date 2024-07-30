5409b710ee4d2961f31260101c482413b07cf2b7
Xaca Rana <xacarana@gmail.com>
2024-07-30 14:43:14 +0000

    instrucciones de instalacion

--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-# Prueba
+# Cambio de idioma sitio web
+
+En este ejemplo se usa (polyglot.js)[https://www.npmjs.com/package/node-polyglot] para realizar la traducción de idioma de un sitio web.
+
+Para ver el ejemplo, seguir los siguientes pasos en la tareminal:
+
+- Clonar el repositorio ``git clone https://github.com/xaca/cambio_idioma.git``
+- Entrar a la carpeta del proyecto ``cd cambio_idioma``
+- Instalar dependencias ``npm i``
+- Ejecutar el proyecto en modo desarrollo ``npm run dev``
+- Abrir el sitio en localhost, como se indique en la consola
--- src/js/xaca/i18n/idioma.js
+++ src/js/xaca/i18n/idioma.js
@@ -12,11 +12,13 @@ window.onload = function() {
   btn_en = document.getElementById("btn_en");
   btn_es = document.getElementById("btn_es");
   control_idioma();
-  btn_en.addEventListener("click",(event)=>{
-    console.log(event);
-  });
+  btn_en.addEventListener("click",actualizarIdioma);
+  btn_es.addEventListener("click",actualizarIdioma);
 }
-
+function actualizarIdioma(event){
+  //console.log(event.target.attributes[1].nodeValue);
+  cambiarIdioma(event.target.attributes[1].nodeValue);
+}
 function control_idioma(){
     polyglot = new Polyglot();
     polyglot.extend(getLocale());
@@ -63,70 +65,17 @@ function getLocale() {
 
   function cargarTextos(){
     
-    document.getElementById("tp_menu_1").innerHTML = polyglot.t("tp_menu_1");
-    document.getElementById("tp_menu_2").innerHTML = polyglot.t("tp_menu_2");
-    document.getElementById("tp_menu_3").innerHTML = polyglot.t("tp_menu_3");
-    document.getElementById("tp_menu_4").innerHTML = polyglot.t("tp_menu_4");
-    document.getElementById("tp-bn-h4-1").innerHTML = polyglot.t("tp-bn-h4-1");
-    document.getElementById("tp-bn-h4-2").innerHTML = polyglot.t("tp-bn-h4-2");
-    document.getElementById("tp-bn-h4-3").innerHTML = polyglot.t("tp-bn-h4-3");
-    document.getElementById("tp-bn-p-1").innerHTML = polyglot.t("tp-bn-p-2");
-    document.getElementById("tp-bn-p-3").innerHTML = polyglot.t("tp-bn-p-3");
-    document.getElementById("tp-btn").innerHTML = polyglot.t("tp-btn");
-    document.getElementById("tp-form-1").innerHTML = polyglot.t("tp-form-1");
-    document.getElementById("tp-txt-form").innerHTML = polyglot.t("tp-txt-form");
-    document.getElementById("tp-services-hd-title").innerHTML = polyglot.t("tp-services-hd-title");
-    document.getElementById("tp-services-hd-span").innerHTML = polyglot.t("tp-services-hd-span");
-    document.getElementById("tp-services-hd-title-1").innerHTML = polyglot.t("tp-services-hd-title-1");
-    document.getElementById("tp-services-hd-title-2").innerHTML = polyglot.t("tp-services-hd-title-2");
-    document.getElementById("tp-services-hd-title-3").innerHTML = polyglot.t("tp-services-hd-title-3");
-    document.getElementById("tp-hd-more-info-title").innerHTML = polyglot.t("tp-hd-more-info-title");
-    document.getElementById("tp-hd-more-info-p").innerHTML = polyglot.t("tp-hd-more-info-p");
-    document.getElementById("tp-conf-title").innerHTML = polyglot.t("tp-conf-title");
-    document.getElementById("tp-callback-title").innerHTML = polyglot.t("tp-callback-title");
-    document.getElementById("tp-callback-p").innerHTML = polyglot.t("tp-callback-p");
-    document.getElementById("tp-more-info-title").innerHTML = polyglot.t("tp-more-info-title");
-    document.getElementById("tp-team-section-heading-title").innerHTML = polyglot.t("tp-team-section-heading-title");
-    document.getElementById("tp-team-section-heading-span").innerHTML = polyglot.t("tp-team-section-heading-span");
-    document.getElementById("tp-team-item-h4-1").innerHTML = polyglot.t("tp-team-item-h4-1");
-    document.getElementById("tp-team-item-span-1").innerHTML = polyglot.t("tp-team-item-span-1");
-    document.getElementById("tp-team-item-p-1").innerHTML = polyglot.t("tp-team-item-p-1");
-    document.getElementById("tp-team-item-h4-2").innerHTML = polyglot.t("tp-team-item-h4-2");
-    document.getElementById("tp-team-item-span-2").innerHTML = polyglot.t("tp-team-item-span-2");
-    document.getElementById("tp-team-item-p-2").innerHTML = polyglot.t("tp-team-item-p-2");
-    document.getElementById("tp-team-item-h4-3").innerHTML = polyglot.t("tp-team-item-h4-3");
-    document.getElementById("tp-team-item-span-3").innerHTML = polyglot.t("tp-team-item-span-3");
-    document.getElementById("tp-teak-item-p-3").innerHTML = polyglot.t("tp-teak-item-p-3");
-    document.getElementById("tp-single-services-title-h1").innerHTML = polyglot.t("tp-single-services-title-h1");
-    document.getElementById("tp-single-services-title-1").innerHTML = polyglot.t("tp-single-services-title-1");
-    document.getElementById("tp-single-services-title-2").innerHTML = polyglot.t("tp-single-services-title-2");
-    document.getElementById("tp-single-services-title-3").innerHTML = polyglot.t("tp-single-services-title-3");
-    document.getElementById("tp-single-services-tab-1-li-1").innerHTML = polyglot.t("tp-single-services-tab-1-li-1");
-    document.getElementById("tp-single-services-tab-1-li-2").innerHTML = polyglot.t("tp-single-services-tab-1-li-2");
-    document.getElementById("tp-single-services-tab-1-li-3").innerHTML = polyglot.t("tp-single-services-tab-1-li-3");
-    document.getElementById("tp-single-services-tab-1-li-4").innerHTML = polyglot.t("tp-single-services-tab-1-li-4");
-    document.getElementById("tp-single-services-tab-1-li-5").innerHTML = polyglot.t("tp-single-services-tab-1-li-5");
-    document.getElementById("tp-single-services-tab-1-li-6").innerHTML = polyglot.t("tp-single-services-tab-1-li-6");
-    document.getElementById("tp-single-services-tab-1-li-7").innerHTML = polyglot.t("tp-single-services-tab-1-li-7");
-    document.getElementById("tp-single-services-tab-2-li-1").innerHTML = polyglot.t("tp-single-services-tab-2-li-1");
-    document.getElementById("tp-single-services-tab-2-li-2").innerHTML = polyglot.t("tp-single-services-tab-2-li-2");
-    document.getElementById("tp-single-services-tab-2-li-3").innerHTML = polyglot.t("tp-single-services-tab-2-li-3");
-    document.getElementById("tp-single-services-tab-2-li-4").innerHTML = polyglot.t("tp-single-services-tab-2-li-4");
-    document.getElementById("tp-single-services-tab-2-li-5").innerHTML = polyglot.t("tp-single-services-tab-2-li-5");
-    document.getElementById("tp-single-services-tab-3-li-1").innerHTML = polyglot.t("tp-single-services-tab-3-li-1");
-    document.getElementById("tp-single-services-tab-3-li-2").innerHTML = polyglot.t("tp-single-services-tab-3-li-2");
-    document.getElementById("tp-single-services-tab-3-li-3").innerHTML = polyglot.t("tp-single-services-tab-3-li-3");
-    document.getElementById("tp-single-services-callback-title").innerHTML = polyglot.t("tp-single-services-callback-title");
-    document.getElementById("tp-contact-title").innerHTML = polyglot.t("tp-contact-title");
-    document.getElementById("tp-contact-span").innerHTML = polyglot.t("tp-contact-span");
-    document.getElementById("tp-contact-item-p").innerHTML = polyglot.t("tp-contact-item-p");
-    document.getElementById("tp-contact-item-h4").innerHTML = polyglot.t("tp-contact-item-h4");
-    document.getElementById("tp-form-name").innerHTML = polyglot.t("tp-form-name");
-    document.getElementById("tp-form-email").innerHTML = polyglot.t("tp-form-email");
-    document.getElementById("tp-form-message").innerHTML = polyglot.t("tp-form-message");
-    document.getElementById("tp-form-submit").innerHTML = polyglot.t("tp-form-submit");
-    document.getElementById("tp-footer-contact-title").innerHTML = polyglot.t("tp-footer-contact-title");
-    document.getElementById("tp-footer-contact-p").innerHTML = polyglot.t("tp-footer-contact-p");
+    try{
+      document.getElementById("xaca_menu_1").innerHTML = polyglot.t("xaca_menu_1");
+      document.getElementById("xaca_menu_2").innerHTML = polyglot.t("xaca_menu_2");
+      document.getElementById("xaca_menu_3").innerHTML = polyglot.t("xaca_menu_3");
+      document.getElementById("xaca_menu_4").innerHTML = polyglot.t("xaca_menu_4");
+      document.getElementById("xaca_titulo").innerHTML = polyglot.t("xaca_titulo");
+      document.getElementById("xaca_texto").innerHTML = polyglot.t("xaca_texto");
+    }catch(e){
+      console.warn("Polyglot",e);
+    }
+    
 
     
   }
